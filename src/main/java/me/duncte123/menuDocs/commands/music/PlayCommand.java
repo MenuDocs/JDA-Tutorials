0babb509f59f8cd23aae505cb92dd196689f6411
Duncan Sterken <duncan.sterken123@gmail.com>
2019-02-17 15:24:04 +0100

    Ep 12 OwO

--- src/main/java/me/duncte123/menuDocs/commands/music/PlayCommand.java
+++ src/main/java/me/duncte123/menuDocs/commands/music/PlayCommand.java
@@ -1,24 +1,55 @@
 package me.duncte123.menuDocs.commands.music;
 
+import me.duncte123.menuDocs.Constants;
 import me.duncte123.menuDocs.music.PlayerManager;
 import me.duncte123.menuDocs.objects.ICommand;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class PlayCommand implements ICommand {
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
+
+        TextChannel channel = event.getChannel();
+
+        if (args.isEmpty()) {
+            channel.sendMessage("Please provide some arguments").queue();
+
+            return;
+        }
+
+        String input = String.join(" ", args);
+
+        if (!isUrl(input) && !input.startsWith("ytsearch:")) {
+            // Use the youtube api for search instead, making a lot of requests with "ytsearch:" will get you blocked
+            channel.sendMessage("Please provide a valid youtube, soundcloud or bandcamp link").queue();
+
+            return;
+        }
+
         PlayerManager manager = PlayerManager.getInstance();
 
-        manager.loadAndPlay(event.getChannel(), "https://youtu.be/b6QHGfphoXU");
+        manager.loadAndPlay(event.getChannel(), input);
+    }
 
-        manager.getGuildMusicManager(event.getGuild()).player.setVolume(10);
+    private boolean isUrl(String input) {
+        try {
+            new URL(input);
+
+            return true;
+        } catch (MalformedURLException ignored) {
+            return false;
+        }
     }
 
     @Override
     public String getHelp() {
-        return null;
+        return "Plays a song\n" +
+                "Usage: `" + Constants.PREFIX + getInvoke() + " <song url>`";
     }
 
     @Override
