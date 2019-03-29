b288b4564f29e63cc4a25f2f88dadc18295494c0
Duncan Sterken <duncan.sterken123@gmail.com>
2019-03-29 16:08:54 +0100

    Create episdoe 16

---
+++ src/main/java/me/duncte123/menuDocs/commands/music/NowPlayingCommand.java
@@ -0,0 +1,58 @@
+package me.duncte123.menuDocs.commands.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import me.duncte123.botcommons.messaging.EmbedUtils;
+import me.duncte123.menuDocs.music.GuildMusicManager;
+import me.duncte123.menuDocs.music.PlayerManager;
+import me.duncte123.menuDocs.objects.ICommand;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class NowPlayingCommand implements ICommand {
+    @Override
+    public void handle(List<String> args, GuildMessageReceivedEvent event) {
+        TextChannel channel = event.getChannel();
+        PlayerManager playerManager = PlayerManager.getInstance();
+        GuildMusicManager musicManager = playerManager.getGuildMusicManager(event.getGuild());
+        AudioPlayer player = musicManager.player;
+
+        if (player.getPlayingTrack() == null) {
+            channel.sendMessage("The player is not playing any song.").queue();
+
+            return;
+        }
+
+        AudioTrackInfo info = player.getPlayingTrack().getInfo();
+
+        channel.sendMessage(EmbedUtils.embedMessage(String.format(
+                "**Playing** [%s](%s)\n%s %s - %s",
+                info.title,
+                info.uri,
+                player.isPaused() ? "\u23F8" : "▶",
+                formatTime(player.getPlayingTrack().getPosition()),
+                formatTime(player.getPlayingTrack().getDuration())
+        ))).queue();
+    }
+
+    @Override
+    public String getHelp() {
+        return "Shows the currently playing track";
+    }
+
+    @Override
+    public String getInvoke() {
+        return "np";
+    }
+
+    private String formatTime(long timeInMillis) {
+        final long hours = timeInMillis / TimeUnit.HOURS.toMillis(1);
+        final long minutes = timeInMillis / TimeUnit.MINUTES.toMillis(1);
+        final long seconds = timeInMillis % TimeUnit.MINUTES.toMillis(1) / TimeUnit.SECONDS.toMillis(1);
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+}
